cca93c042760d5e7f5e972f1d7ba2ba23373100c
M. Fahrial Agustian <54786431+fahrialagustian@users.noreply.github.com>
2021-03-16 20:21:44 +0800

    up

--- admin/izin/index.php
+++ admin/izin/index.php
@@ -1,4 +1,116 @@
 <?php
+if (isset($_GET['cetak'])) {
+    include '../../config.php';
+    include '../../tgl_helper.php';
+    $query = mysqli_query($koneksi, "SELECT * FROM pegawai where id_jabatan='3'") or die(mysqli_error($koneksi));
+    $kepala = mysqli_fetch_array($query);
+
+    $data = '';
+    $no = 1;
+    if ($_GET['tahun'] == '' and $_GET['bulan'] == '' and $_GET['status']=='') {
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+    } else if ($_GET['tahun'] != '' and $_GET['status'] !='') {
+        $y = $_GET['tahun'];
+        $status = $_GET['status'];
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+
+    } else if ($_GET['bulan'] != '' and $_GET['status']!='') {
+        $m = $_GET['bulan'];
+        $status = $_GET['status'];
+
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and MONTH(izin.tgl_buat)='$m' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+    } else if (
+        $_GET['bulan'] != '' and $_GET['tahun'] != ''
+    ) {
+        $m = $_GET['bulan'];
+        $y = $_GET['tahun'];
+
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where MONTH(izin.tgl_buat)='$m' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }  else {
+        $y = $_GET['tahun'];
+        $m = $_GET['bulan'];
+        $status = $_GET['status'];
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and MONTH(izin.tgl_buat)='$m' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }
+    // 
+    while ($dt = mysqli_fetch_array($sql)) {
+
+        $jum = mysqli_num_rows($query);
+        $data .= '<tr><td>' . $no . '</td><td>' . $dt['nama_pegawai'] . '<br><small>NIP.' . $dt['nip'] . '</small><td style="text-align: left;"> ' . tgl_indo($dt['tgl_buat']) . '</td><td style="text-align: left;"> ' . $dt['alasan'] . '</td><td>' . tgl_indo($dt['tgl_awal']) . '</td><td>' . tgl_indo($dt['tgl_akhir']) . '</td><td style="text-align: left;"> ' . $dt['status'] . '</td>  </tr>';
+        $no++;
+    }
+
+
+    include '../../vendor/autoload.php';
+    $html = '
+<table>
+    <tr>
+        <td><img style="width:15%" src="logo.jpg"></td>
+        <td style="text-align:center; width:570px">
+            <h4>KEMENTERIAN AGAMA RI</h4>
+            <h4>KANTOR KEMENTERIAN AGAMA KAB. TANAH LAUT</h4>
+            <h5>MADRASAH TSANAWIYAH NEGERI 4 TANAH LAUT</h5>
+            <p>Jl. Pengeran Antasari Telp. (5012) 21695 Pelaihari</p>
+            <p>Kab. Tanah Laut</p>
+            <p>Email : mtsn4pelaihari@gmail.com</p>
+        
+        </td>
+    </tr>
+</table>
+<hr size="100px" width="500px">
+
+
+<h3 style="text-align:center;">Data Rekap Izin</h3>
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-baqh{text-align:center;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+<table class="tg" width="100%">
+<thead>
+  <tr>
+    <th class="tg-baqh">No</th>
+    <th class="tg-baqh">Nama</th>
+    <th class="tg-baqh">Tgl. Buat</th>
+    <th class="tg-baqh">Alasan</th>
+    <th class="tg-baqh">Tgl. Awal</th>
+    <th class="tg-baqh">Tgl. Akhir</th>
+    <th class="tg-baqh">Status</th>
+  </tr>
+</thead>
+<tbody>
+' . $data . '
+</tbody>
+</table>
+<br>
+<br><br>
+<table >
+    <tr>
+        <td style="text-align:center; width:90%"></td>
+        <td style="text-align:center; width:50%">
+            <p>Pelaihari,' . tgl_indo(date('Y-m-d')) . '</p>
+            <p>Kepala Sekolah</p>
+            <p>MTsN 4 TANAH LAUT</p>
+            <br>
+            <br>
+            <br>
+            <p>' . $kepala['nama_pegawai'] . '</p>
+            <p>NIP. ' . $kepala['nip'] . '</p>
+        
+        </td>
+    </tr>
+</table>
+
+';
+    $mpdf = new \Mpdf\Mpdf();
+    $mpdf->WriteHTML($html);
+    $mpdf->Output();
+}
 include "../komponen/head.php";
 ?>
 <?php
@@ -38,17 +150,49 @@ include "../komponen/menu.php";
                                 <div class="col-3">
                                     <form method="GET" action="">
                                         <div class="form-group">
-                                            <select class="form-control select2" name="status" style="width: 100%;">
-                                                <option selected="selected" disabled>--Pilih Status--</option>
-                                                    <option value="Proses">Proses</option>
-                                                    <option value="Acc">Acc</option>
-                                                    <option value="Tolak">Tolak</option>
-                                                    <option value="">Semua</option>
+                                            <select class="form-control select2" name="bulan" style="width: 100%;">
+                                                <option selected="selected" value="">--Pilih Bulan--</option>
+                                                <?php
+                                                $sql = mysqli_query($koneksi, "SELECT MONTH(tgl_buat) as bulan from izin GROUP by MONTH(tgl_buat) ASC") or die(mysqli_error($koneksi));
+                                                while ($bulan = mysqli_fetch_array($sql)) {
+                                                ?>
+                                                    <option value="<?php echo $bulan['bulan'] ?>"> <?php echo getBulan($bulan['bulan']) ?></option>
+                                                <?php
+                                                }
+                                                ?>
                                             </select>
                                         </div>
                                 </div>
+                                <div class="col-2">
+                                    <div class="form-group">
+                                        <select class="form-control select2" name="tahun" style="width: 100%;">
+                                            <option selected="selected" value="">--Pilih Tahun--</option>
+                                            <?php
+                                            $sql = mysqli_query($koneksi, "SELECT Year(tgl_buat) as tahun from izin GROUP by Year(tgl_buat) ASC") or die(mysqli_error($koneksi));
+                                            while ($tahun = mysqli_fetch_array($sql)) {
+                                            ?>
+                                                <option value="<?php echo $tahun['tahun'] ?>"> <?php echo $tahun['tahun'] ?></option>
+                                            <?php
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="form-group">
+                                        <select class="form-control select2" name="status" style="width: 100%;">
+                                            <option selected="selected" value="">--Pilih Status--</option>
+                                            <option value="Proses">Proses</option>
+                                            <option value="Acc">Acc</option>
+                                            <option value="Tolak">Tolak</option>
+                                            <option value="">Semua</option>
+                                        </select>
+                                    </div>
+                                </div>
                                 <div class="col-5">
                                     <button name="filter" class="btn btn-success"><i class="fa fa-search"></i> Filter</button>
+
+                                    <button name="cetak" class="btn btn-default"><i class="fa fa-print"></i> Cetak</button>
                                     <a href="tambah_izin.php"><button type="button" class="btn btn-primary"><i class="fa fa-plus"></i> Izin</button></a>
                                 </div>
                                 </form>
@@ -73,18 +217,37 @@ include "../komponen/menu.php";
                                 <tbody>
                                     <?php
                                     $no = 1;
-                                    if(isset($_GET['filter'])){
+                                    if (isset($_GET['filter'])) {
                                         $status = $_GET['status'];
-                                        if ($status =='') {
+                                        if ($_GET['tahun'] == '' and $_GET['bulan'] == '' and $_GET['status'] == '') {
                                             $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+                                        } else if ($_GET['tahun'] != '' and $_GET['status'] != '') {
+                                            $y = $_GET['tahun'];
+                                            $status = $_GET['status'];
+                                            $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+                                        } else if ($_GET['bulan'] != '' and $_GET['status'] != ''
+                                        ) {
+                                            $m = $_GET['bulan'];
+                                            $status = $_GET['status'];
+
+                                            $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and MONTH(izin.tgl_buat)='$m' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+                                        } else if (
+                                            $_GET['bulan'] != '' and $_GET['tahun'] != ''
+                                        ) {
+                                            $m = $_GET['bulan'];
+                                            $y = $_GET['tahun'];
+
+                                            $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where MONTH(izin.tgl_buat)='$m' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
                                         } else {
-                                            $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
+                                            $y = $_GET['tahun'];
+                                            $m = $_GET['bulan'];
+                                            $status = $_GET['status'];
+                                            $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip where izin.status='$status' and MONTH(izin.tgl_buat)='$m' and Year(izin.tgl_buat)='$y' ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
                                         }
-                                        
-                                    }else{
+                                    } else {
                                         $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai, izin.alasan,izin.tgl_awal,izin.tgl_akhir,izin.keterangan,izin.status,izin.id_izin, izin.tgl_buat from pegawai join izin on pegawai.nip=izin.nip ORDER BY izin.tgl_buat DESC") or die(mysqli_error($koneksi));
                                     }
-                                    
+
 
                                     while ($dt = mysqli_fetch_array($sql)) {
                                     ?>
--- admin/sppd/index.php
+++ admin/sppd/index.php
@@ -1,4 +1,105 @@
 <?php
+if (isset($_GET['cetak'])) {
+    include '../../config.php';
+    include '../../tgl_helper.php';
+    $query = mysqli_query($koneksi, "SELECT * FROM pegawai where id_jabatan='3'") or die(mysqli_error($koneksi));
+    $kepala = mysqli_fetch_array($query);
+
+    $data ='';
+    $no = 1; 
+    if($_GET['tahun'] =='' and $_GET['bulan'] ==''){
+        $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip  ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }
+    else if($_GET['tahun'] !=''){
+        $y = $_GET['tahun'];
+       $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where  YEAR(sppd.tgl_buat)='$y' ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }else if($_GET['bulan'] != ''){
+        $m = $_GET['bulan'];
+       $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where MONTH(sppd.tgl_buat)='$m' ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }else{
+        $y = $_GET['tahun'];
+        $m = $_GET['bulan'];
+       $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where MONTH(sppd.tgl_buat)='$m' and YEAR(sppd.tgl_buat)='$y' ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
+    }
+    // 
+    while ($dt = mysqli_fetch_array($sql)) {
+        
+            $jum = mysqli_num_rows($query);
+            $data .='<tr><td>'.$no .'</td><td>'.$dt['nama_pegawai'].'<br><small>NIP.'.$dt['nip'] .'</small><td style="text-align: left;"> '.tgl_indo($dt['tgl_buat']).'</td><td style="text-align: left;"> '.$dt['maksud'].'</td><td style="text-align: right;"> '.$dt['tujuan'].'</td><td>'.tgl_indo($dt['tgl_berngkat']).'</td><td>'.tgl_indo($dt['tgl_pulang']).'</td>  </tr>';
+            $no++;
+    }
+
+
+include '../../vendor/autoload.php';
+$html = '
+<table>
+    <tr>
+        <td><img style="width:15%" src="logo.jpg"></td>
+        <td style="text-align:center; width:570px">
+            <h4>KEMENTERIAN AGAMA RI</h4>
+            <h4>KANTOR KEMENTERIAN AGAMA KAB. TANAH LAUT</h4>
+            <h5>MADRASAH TSANAWIYAH NEGERI 4 TANAH LAUT</h5>
+            <p>Jl. Pengeran Antasari Telp. (5012) 21695 Pelaihari</p>
+            <p>Kab. Tanah Laut</p>
+            <p>Email : mtsn4pelaihari@gmail.com</p>
+        
+        </td>
+    </tr>
+</table>
+<hr size="100px" width="500px">
+
+
+<h3 style="text-align:center;">Data Rekap SPPD</h3>
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-baqh{text-align:center;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+<table class="tg" width="100%">
+<thead>
+  <tr>
+    <th class="tg-baqh">No</th>
+    <th class="tg-baqh">Nama</th>
+    <th class="tg-baqh">Tanggal</th>
+    <th class="tg-baqh">Maksud</th>
+    <th class="tg-baqh">Tujuan</th>
+    <th class="tg-baqh">Tgl. Berangkat</th>
+    <th class="tg-baqh">Tgl. Pulang</th>
+  </tr>
+</thead>
+<tbody>
+'.$data. '
+</tbody>
+</table>
+<br>
+<br><br>
+<table >
+    <tr>
+        <td style="text-align:center; width:90%"></td>
+        <td style="text-align:center; width:50%">
+            <p>Pelaihari,'.tgl_indo(date('Y-m-d')). '</p>
+            <p>Kepala Sekolah</p>
+            <p>MTsN 4 TANAH LAUT</p>
+            <br>
+            <br>
+            <br>
+            <p>'.$kepala['nama_pegawai'].'</p>
+            <p>NIP. '. $kepala['nip'].'</p>
+        
+        </td>
+    </tr>
+</table>
+
+';
+ $mpdf = new \Mpdf\Mpdf();
+   $mpdf->WriteHTML($html);
+    $mpdf->Output();
+}
 include "../komponen/head.php";
 ?>
 <?php
@@ -38,7 +139,7 @@ include "../komponen/menu.php";
                                     <form method="GET" action="">
                                         <div class="form-group">
                                             <select class="form-control select2"  name="bulan" style="width: 100%;">
-                                                <option selected="selected" disabled>--Pilih Bulan--</option>
+                                                <option selected="selected" value="">--Pilih Bulan--</option>
                                                 <?php
                                                 $sql = mysqli_query($koneksi, "SELECT MONTH(tgl_buat) as bulan from sppd GROUP by MONTH(tgl_buat) ASC") or die(mysqli_error($koneksi));
                                                 while ($bulan = mysqli_fetch_array($sql)) {
@@ -53,7 +154,7 @@ include "../komponen/menu.php";
                                 <div class="col-2">
                                     <div class="form-group">
                                         <select class="form-control select2" name="tahun" style="width: 100%;">
-                                            <option selected="selected" disabled>--Pilih Tahun--</option>
+                                            <option selected="selected" value="">--Pilih Tahun--</option>
                                             <?php
                                             $sql = mysqli_query($koneksi, "SELECT Year(tgl_buat) as tahun from sppd GROUP by Year(tgl_buat) ASC") or die(mysqli_error($koneksi));
                                             while ($tahun = mysqli_fetch_array($sql)) {
@@ -67,6 +168,7 @@ include "../komponen/menu.php";
                                 </div>
                                 <div class="col-5">
                                     <button name="filter" class="btn btn-success"><i class="fa fa-search"></i> Filter</button>
+                                    <button name="cetak" class="btn btn-default"><i class="fa fa-print"></i> Cetak</button>
                                     <a href="tambah_sppd.php"><button type="button" class="btn btn-primary"><i class="fa fa-plus"></i> SPPD</button></a>
                                 </div>
                                 </form>
@@ -91,9 +193,27 @@ include "../komponen/menu.php";
                                 <?php
                                 $no = 1;
                                 if(isset($_GET['filter'])){
-                                    $m = $_GET['bulan'];
-                                    $y = $_GET['tahun'];
-                                    $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where MONTH(sppd.tgl_buat)='$m' and YEAR(sppd.tgl_buat)='$y' ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
+                                    if ($_GET['tahun'] == '' and $_GET['bulan'] == '') {
+                                        $sql = mysqli_query($koneksi,
+                                            "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip  ORDER BY sppd.tgl_buat DESC"
+                                        ) or die(mysqli_error($koneksi));
+                                    } else if ($_GET['tahun'] != '') {
+                                        $y = $_GET['tahun'];
+                                        $sql = mysqli_query($koneksi,
+                                            "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where  YEAR(sppd.tgl_buat)='$y' ORDER BY sppd.tgl_buat DESC"
+                                        ) or die(mysqli_error($koneksi));
+                                    } else if ($_GET['bulan'] != '') {
+                                        $m = $_GET['bulan'];
+                                        $sql = mysqli_query($koneksi,
+                                            "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where MONTH(sppd.tgl_buat)='$m' ORDER BY sppd.tgl_buat DESC"
+                                        ) or die(mysqli_error($koneksi));
+                                    } else {
+                                        $y = $_GET['tahun'];
+                                        $m = $_GET['bulan'];
+                                        $sql = mysqli_query($koneksi,
+                                            "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat,sppd.tgl_berngkat,sppd.tgl_pulang,sppd.tujuan FROM pegawai JOIN sppd on pegawai.nip=sppd.nip where MONTH(sppd.tgl_buat)='$m' and YEAR(sppd.tgl_buat)='$y' ORDER BY sppd.tgl_buat DESC"
+                                        ) or die(mysqli_error($koneksi));
+                                    }
                                 }else{
                                     $sql = mysqli_query($koneksi, "SELECT pegawai.nip,pegawai.nama_pegawai,sppd.maksud,sppd.id_sppd,sppd.status,sppd.tgl_buat FROM pegawai JOIN sppd on pegawai.nip=sppd.nip ORDER BY sppd.tgl_buat DESC") or die(mysqli_error($koneksi));
                                 }
